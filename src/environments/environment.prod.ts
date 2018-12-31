a8e2a7ec92e39287b2fb859debed4a0dbfb46049
Nivedita Gautam <niveditagautam000@gmail.com>
2018-12-31 02:26:32 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDzYik0QbjxHPRpDKsvbw3JLWFFjtikQw0",
+    authDomain: "poll-app-c7320.firebaseapp.com",
+    databaseURL: "https://poll-app-c7320.firebaseio.com",
+    projectId: "poll-app-c7320",
+    storageBucket: "poll-app-c7320.appspot.com",
+    messagingSenderId: "573904472043"
+  }
 };
